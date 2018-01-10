9ccb0e717dfda5f082d7eebbcedf821101dd3099
Abdul Wakeel <awkhan978@gmail.com>
2018-01-10 14:46:06 +0500

    add database module

--- server/app/views/common.py
+++ server/app/views/common.py
@@ -1,8 +1,33 @@
+import json
+
 from flask import request
 from server.app import app
 from server.app.models import common
 
+var = [{
+    "Categories": {
+        "Sports & Outdoors": {
+            "Fan Shop": {
+                "Auto Accessories": {
+                    "Air Fresheners": {},
+                    "Antenna Toppers": {},
+                    "Car Covers": {}
+                },
+                "Bags, Packs & Accessories": {}
 
-@app.route('/test')
+            },
+            "Outdoor Recreation": {},
+            "Sports & Fitness": {}
+        }
+    }
+}]
+
+
+@app.route('/')
 def test():
-    return common.MESSAGES["Test"]
+    return "Crawper Server routes working"
+
+
+@app.route('/categories')
+def return_categories():
+    return json.dumps(var)
