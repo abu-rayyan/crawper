a3ccea95daa7b4cabc9f31d45d8a83a4210df20b
Abdul Wakeel <awkhan978@gmail.com>
2018-01-10 15:41:35 +0500

    add mock server

---
+++ server/mock/categories.py
@@ -0,0 +1,51 @@
+categories = {
+    "Categories": {
+        "Sports & Outdoors": {
+            "Fan Shop": {
+                "Auto Accessories": {
+                    "Air Fresheners": {},
+                    "Antenna Toppers": {},
+                    "Car Covers": {},
+                    "Car Flags": {},
+                    "Car Magnets": {},
+                    "Decals & Bumper Stickers": {},
+                    "Emblems": {},
+                    "Floormats": {},
+                    "Key Chains": {},
+                    "License Plate Covers": {},
+                    "License Plate Frames": {}
+                },
+                "Bags, Packs & Accessories": {
+                    "Backpacks": {},
+                    "Drawstring Bags": {},
+                    "Duffle Bags": {},
+                    "Laptop & Messenger Bags": {}
+                },
+                "Caps & Hats": {},
+                "Cell Phone Accessories": {},
+                "Clothing": {},
+                "Clothing Accessories": {},
+                "Electronics": {}
+            },
+            "Outdoor Recreation": {
+                "Outdoor Clothing": {},
+                "Camping & Hiking": {},
+                "Climbing": {},
+                "Cycling": {},
+                "Skates, Skateboards & Scooters": {},
+                "Water Sports": {},
+                "Winter Sports": {},
+                "Accessories": {}
+            },
+            "Sports & Fitness": {}
+        },
+        "Video Games": {
+            "PlayStation 4": {},
+            "PlayStation 3": {},
+            "Xbox One": {},
+            "Xbox 360": {}
+        },
+        "Toys & Games": {},
+        "Baby": {}
+    }
+}
