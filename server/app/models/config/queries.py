eba75db8d63796a263a386d5f63207da722b62dc
awakeel <awkhan978@gmail.com>
2018-02-22 11:59:27 +0500

    fix search query

--- server/app/models/config/queries.py
+++ server/app/models/config/queries.py
@@ -8,7 +8,7 @@ QUERIES = {
     "GetProduct": "SELECT product_asin, product_title, product_price, product_link, total_reviews, product_rank, "
                   "image_link, product_rating FROM crawper.products WHERE product_asin=%s AND category_name=%s; ",
     "SearchProduct": "SELECT product_asin, product_title, product_price, total_reviews, image_link, product_link, "
-                     "category_name FROM crawper.products Where product_title LIKE %s;",
+                     "category_name FROM crawper.products Where product_title ILIKE %s;",
     "GetSentimentLabels": "SELECT reviews_analysis.sentiment_label FROM crawper.reviews, crawper.reviews_analysis "
                           "WHERE crawper.reviews.review_link = crawper.reviews_analysis.review_link AND "
                           "crawper.reviews.product_asin=%s; ",
