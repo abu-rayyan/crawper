8beb43282c2a33fa7670e8d2ae0eaaf7fc071b98
sameershewakram <sameershewakram@gmail.com>
2018-03-05 22:44:34 +0500

    rm some urls

--- src/common/config/urls.py
+++ src/common/config/urls.py
@@ -9,16 +9,7 @@ Products = {
         'Computer Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Accessories/zgbs/sporting-goods/3318643011/',
         'Headphones': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Headphones/zgbs/sporting-goods/3209247011/',
         'MP3 Player Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-MP3-Player-Accessories/zgbs/sporting-goods/3209248011/',
-        'Tablet Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Tablet-Accessories/zgbs/sporting-goods/13227293011/',
-
-        'Electronics': 'https://www.amazon.com/gp/new-releases/sporting-goods/2311740011/',
-        'Binoculars': 'https://www.amazon.com/gp/new-releases/sporting-goods/375521011/',
-        'Computer Mice': 'https://www.amazon.com/gp/new-releases/sporting-goods/374848011/',
-        'Computer Mousepads': 'https://www.amazon.com/gp/new-releases/sporting-goods/374849011/',
-        'Computer Accessories': 'https://www.amazon.com/gp/new-releases/sporting-goods/3318643011/',
-        'Headphones': 'https://www.amazon.com/gp/new-releases/sporting-goods/3209247011/',
-        'MP3 Player Accessories': 'https://www.amazon.com/gp/new-releases/sporting-goods/3209248011/',
-        'Tablet Accessories': 'https://www.amazon.com/gp/new-releases/sporting-goods/13227293011/',
+        'Tablet Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Tablet-Accessories/zgbs/sporting-goods/13227293011/'
 
         
     }
