e6945d1812e583df5cf371eb305f4751b2a5412f
sameershewakram <sameershewakram@gmail.com>
2018-03-06 12:38:47 +0500

    only Binocular urls

--- src/common/config/urls.py
+++ src/common/config/urls.py
@@ -2,15 +2,7 @@ Products = {
     'NewReleases': {
 
 
-        'Electronics': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Electronics/zgbs/sporting-goods/2311740011/',
         'Binoculars': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Binoculars/zgbs/sporting-goods/375521011/',
-        'Computer Mice': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Mice/zgbs/sporting-goods/374848011/',
-        'Computer Mousepads': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Mousepads/zgbs/sporting-goods/374849011/',
-        'Computer Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Accessories/zgbs/sporting-goods/3318643011/',
-        'Headphones': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Headphones/zgbs/sporting-goods/3209247011/',
-        'MP3 Player Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-MP3-Player-Accessories/zgbs/sporting-goods/3209248011/',
-        'Tablet Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Tablet-Accessories/zgbs/sporting-goods/13227293011/'
-
-        
+        'Binoculars': 'https://www.amazon.com/gp/new-releases/sporting-goods/375521011/'
     }
 }
