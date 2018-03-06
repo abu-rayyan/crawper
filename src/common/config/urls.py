50284d80f790bbcd95f821175f28cc94e28f5918
sameershewakram <sameershewakram@gmail.com>
2018-03-06 12:47:39 +0500

    binoculars

--- src/common/config/urls.py
+++ src/common/config/urls.py
@@ -2,7 +2,6 @@ Products = {
     'NewReleases': {
 
 
-        'Binoculars': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Binoculars/zgbs/sporting-goods/375521011/',
-        'Binoculars': 'https://www.amazon.com/gp/new-releases/sporting-goods/375521011/'
+        'Binoculars': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Binoculars/zgbs/sporting-goods/375521011/'
     }
 }
