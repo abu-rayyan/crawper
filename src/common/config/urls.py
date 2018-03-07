e368301dfa612d9b2366956ed0176a9aaee8e5d3
sameershewakram <sameershewakram@gmail.com>
2018-03-07 12:17:04 +0500

    add urls and proxies

--- src/common/config/urls.py
+++ src/common/config/urls.py
@@ -1,7 +1,14 @@
 Products = {
     'NewReleases': {
 
-        'Computer Mice': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Mice/zgbs/sporting-goods/374848011/',
-        'Computer Mousepads': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Mousepads/zgbs/sporting-goods/374849011/'
+         'Tablet Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Tablet-Accessories/zgbs/sporting-goods/13227293011/',
+         'MP3 Player Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-MP3-Player-Accessories/zgbs/sporting-goods/3209248011/',
+         'Computer Mousepads': 'https://www.amazon.com/gp/new-releases/sporting-goods/374849011/',
+         'MP3 Player Accessories': 'https://www.amazon.com/gp/new-releases/sporting-goods/3209248011/',
+         'Tablet Accessories': 'https://www.amazon.com/gp/new-releases/sporting-goods/13227293011/',
+         'Computer Mousepads': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Mousepads/zgbs/sporting-goods/374849011/',
+         'Computer Mice': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Mice/zgbs/sporting-goods/374848011/',
+         'Computer Accessories': 'https://www.amazon.com/Best-Sellers-Sports-Outdoors-Fan-Computer-Accessories/zgbs/sporting-goods/3318643011/'
+         
     }
 }
