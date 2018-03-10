f604ecef5a0044d27f348dcfcbd0d240cb46bec5
sameershewakram <sameershewakram@gmail.com>
2018-03-10 14:05:24 +0500

    add proxies, disable r_engine and add urls

--- main.py
+++ main.py
@@ -120,15 +120,15 @@ def main():
 
     # TODO: Replace if found a better way of sharing same mem space
     # noinspection PyUnusedLocal,SpellCheckingInspection
-    #rotator = ProxyRotator('assets/Proxies.txt')  # used as singleton obj
+    rotator = ProxyRotator('assets/Proxies.txt')  # used as singleton obj
 
     # noinspection SpellCheckingInspection
     rengine_threads = config.get('REngine', 'Max Threads')
     # noinspection SpellCheckingInspection
     crawper_threads = config.get('Crawper', 'Max Threads')
 
-    #start_crawper(crawper_threads)
-    start_rengine(db_conn, rengine_threads)
+    start_crawper(crawper_threads)
+    #start_rengine(db_conn, rengine_threads)
     logger.info('closing database connection')
     print('* closing database pool')
     db_conn.close_pool()
