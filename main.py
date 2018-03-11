80fb869b6cdc33d5c2dbed033cd1788867a5ea2b
sameershewakram <sameershewakram@gmail.com>
2018-03-11 13:40:23 +0500

    purge error

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ def main():
     # noinspection SpellCheckingInspection
     #crawper_threads = config.get('Crawper', 'Max Threads')
 
-    start_crawper(crawper_threads)
-    #start_rengine(db_conn, rengine_threads)
+    #start_crawper(crawper_threads)
+    start_rengine(db_conn, rengine_threads)
     logger.info('closing database connection')
     print('* closing database pool')
     db_conn.close_pool()
